602b62d94a5fe9a96b4d54898ab9dd965219947c
John Blalock <simpledimplejohn@gmail.com>
2021-04-16 13:02:50 -0700

    add button will display cipher

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 
-$(document).ready(function() {
 
-});
+  $("button#cipher").click(function(){
+    function add(number1, number2) {
+      return number1 + number2;
+    }
+
+
+  })
+  const number1 = parsInt(prompt("Enter your number: "));
+  const number2 = parsInt(prompt("enter another number:"));
+  
