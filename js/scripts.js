28f852bea709fc8ccf1eed88f8ced878d51ca167
John Blalock <simpledimplejohn@gmail.com>
2021-04-18 08:59:33 -0700

    fixed button function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $("button#cipher").click(function() {
-    $("ul#user").prepend("<li>Hello!</li>");
-    $("ul#webpage").prepend("<li>Why hello there!</li>");
+    prompt("text");
   });
-
+});
